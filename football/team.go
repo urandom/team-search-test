8389ca26cddc2b64f281345bd17f83bba53a3c73
Viktor Kojouharov <developer@sugr.org>
2016-10-22 15:36:50 +0300

    add some docs

--- football/team.go
+++ football/team.go
@@ -1,8 +1,12 @@
 package football
 
+// TeamId is the unique identifier of a team.
 type TeamId int
+
+// PlayerId is the unique identifier of a player.
 type PlayerId string
 
+// Team contains basic information about a football team.
 type Team struct {
 	Id         TeamId
 	Name       string
@@ -10,6 +14,7 @@ type Team struct {
 	Players    []PlayerId
 }
 
+// Player contains basic information about a football player.
 type Player struct {
 	Id    PlayerId
 	Name  string
@@ -17,8 +22,12 @@ type Player struct {
 	Teams []TeamId
 }
 
+// TeamRepository allows queries for teams and players.
 type TeamRepository interface {
+	// GetTeams looks for a team given an id.
 	GetTeam(id TeamId) (Team, error)
+	// GetTeamByName looks for a team given a name.
 	GetTeamByName(name string) (Team, error)
+	// GetPlayer looks for a player given a player id.
 	GetPlayer(id PlayerId) (Player, error)
 }
